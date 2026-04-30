14b9230fa72a7f6285004235621c2bbb0cfebdd9
chrisHebs23 <christopher.heberden@gmail.com>
2026-04-30 17:44:30 -0500

    Backend config setup

---
+++ backend/constants.py
@@ -0,0 +1,6 @@
+CORAL_STATIONS = [
+    {"name": "Hawaii", "station": "hawaii"},
+    {"name": "Mauritius", "station": "northern_mauritius"},
+    {"name": "Florida", "station": "southeast_florida"},
+    {"name": "Cape Town", "station": "south_africa"},
+]
